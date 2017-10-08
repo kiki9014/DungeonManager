6c274266423350847d2736e82213374b168a1afd
Hyunjun Kim <kiki9014@gmail.com>
2017-10-08 15:02:55 +0900

    Add Save button

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    Sprite[] font;
+
+	// Use this for initialization
+	protected virtual void Start () {
+        font = Resources.LoadAll<Sprite>("Sprites/Font");
+
+        string buttonName = name;
+        Debug.Log(buttonName);
+        GameObject emptyObj = new GameObject();
+        for (int i = 0; i < buttonName.Length; i++)
+        {
+            var obj = Instantiate(emptyObj, transform.position - new Vector3(((buttonName.Length - 1) / 2 - i) * 0.1f + 0.05f,0,0), transform.rotation, transform);
+            obj.name = buttonName[i].ToString();
+            obj.AddComponent<SpriteRenderer>().sprite = font[buttonName[i] - 'A'];
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : Button {
+
+	// Use this for initialization
+	void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Save");
+    }
+}
